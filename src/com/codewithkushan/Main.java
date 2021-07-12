18b9ad7322393eb6062ce51943ab4c0666b5fffd
Kushanrawindu <kushanrawindu@gmail.com>
2021-07-12 13:14:43 +0530

    learn data types

---
+++ src/com/codewithkushan/Main.java
@@ -0,0 +1,12 @@
+package com.codewithkushan;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 25;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
